b7262b9832abbe47306d33f19174ac97ea55638c
Danila4114 <135273707+Danila4114@users.noreply.github.com>
2023-06-13 21:40:04 +0300

    Add files via upload

---
+++ 123/Program.cs
@@ -0,0 +1,76 @@
+﻿// задача 10
+int number = ReadInt("Введите трехзначное число: ");
+int amount = number.ToString().Length;
+
+if (amount < 3 || amount > 3)
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
+else
+{
+    Console.WriteLine(InCenter(number));
+}
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+int InCenter(int a)
+{
+    
+    int result = ((a / 10) % 10);
+    return result;
+}
+
+// задача 13
+
+int num = ReadInt("Введите число: ");
+int count = number.ToString().Length;
+Console.Write(MakeArray(number, count));
+int MakeArray(int a, int b)
+{
+int result = 0;
+    if (b < 3)
+    {
+        Console.Write("Третьей цифры нет, держи: ");
+    }
+    else
+    {
+        int c = 1;
+        for (int i = b; i > 3; i--)
+        {
+            c = c * 10;
+        }
+
+        result = (a / c) % 10;
+    }
+return result;
+}
+//задача 15
+namespace ConsoleApp3 
+{ 
+    internal class Program 
+    { 
+        static void Main(string[] args) 
+        { 
+            Console.Write(" Нужно узнать, является ли этот день выходным или нет?  "); 
+            while (true) 
+            { 
+                Console.Write("\n Введите номер дня недели  "); 
+                int num = int.Parse(Console.ReadLine()); 
+                if (num >= 1 && num <= 5) 
+                { 
+                    Console.Write(" Нет, это не выходной"); 
+                } 
+ 
+                else if (num >= 6 && num <= 7) 
+                { 
+                    Console.Write(" Да, это выходной"); 
+                } 
+                else Console.Write(" Не существует такого дня недели"); 
+            } 
+        } 
+    } 
+}
